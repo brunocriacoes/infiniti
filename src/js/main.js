964ff84baef582c1570b2333e46b25b4841e7377
brunocriacoes <br.rafael@outlook.com>
2021-01-19 11:22:46 -0300

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -5,18 +5,18 @@ const route = new Route('#/home')
 
 globalThis.app = new APP
 
-route.add( '/peca-por-user', param => {
-    app.peca_detelhe(param[0], param[1])
-} )
-route.add( '/peca-detalhes-por-user', param => {
-    app.peca_detelhe_user(param[0], param[1])
-} )
-route.add( '/entregar', param => {
-    app.set_user(param[0])
-} )
-route.add( '/devolver', param => {
-    app.set_user(param[0])
-} )
+// route.add( '/peca-por-user', param => {
+//     app.peca_detelhe(param[0], param[1])
+// } )
+// route.add( '/peca-detalhes-por-user', param => {
+//     app.peca_detelhe_user(param[0], param[1])
+// } )
+// route.add( '/entregar', param => {
+//     app.set_user(param[0])
+// } )
+// route.add( '/devolver', param => {
+//     app.set_user(param[0])
+// } )
 
 route.render()
 window.onpopstate = () => {
