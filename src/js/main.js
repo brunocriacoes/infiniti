997acc91ca8d69270323bb2cc1ffd59dd71d4871
brunocriacoes <br.rafael@outlook.com>
2021-02-16 16:00:40 -0300

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,23 @@ globalThis.app = app
 app.start()
 app.is_logged()
 
+route.add( '/lista-de-nomes', parans => {
+    app.lista_de_nomes()
+} )
+
+route.add( '/lista-de-pecas', parans => {
+    app.lista_pecas()
+} )
+
+route.add( '/pecas-por-local', parans => {
+    app.locais()
+} )
+
+route.add( '/detalhe-listagem-peca', parans => {
+    console.log( parans )
+    app.estoque(parans[1], 'PEÇA')
+} )
+
 route.render()
 
 window.onpopstate = () => {
